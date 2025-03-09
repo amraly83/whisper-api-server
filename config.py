0c664053d9e6ca0a84a9d0167dbe7297327c72dd
amraly83 <66267712+amraly83@users.noreply.github.com>
2025-03-09 20:32:43 +0300

    update

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    whisper_model_size: str = os.getenv("MODEL_SIZE", "tiny")
+    compute_type: str = os.getenv("COMPUTE_TYPE", "auto")
+    device: str = os.getenv("DEVICE", "cpu")
+    
+    model_config = {
+        'protected_namespaces': ('settings_',)
+    }
+
+settings = Settings()
