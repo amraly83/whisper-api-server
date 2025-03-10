c1b36d9e057ea344e8706abfe7d6af15141bd698
Amr Rady <66267712+amraly83@users.noreply.github.com>
2025-03-10 11:48:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    whisper_model_size: str = os.getenv("MODEL_SIZE", "tiny")
-    compute_type: str = os.getenv("COMPUTE_TYPE", "int8")  # Set to int8 for quantization
-    device: str = os.getenv("DEVICE", "cpu")  # Explicitly set to CPU
-    beam_size: int = int(os.getenv("BEAM_SIZE", 3)) # Added beam_size setting, default to 3
+    whisper_model_size: str = os.getenv("MODEL_SIZE")
+    compute_type: str = os.getenv("COMPUTE_TYPE")  # Set to int8 for quantization
+    device: str = os.getenv("DEVICE")  # Explicitly set to CPU
+    beam_size: int = int(os.getenv("BEAM_SIZE")) # Added beam_size setting, default to 3
 
     model_config = {
         'protected_namespaces': ('settings_',)
