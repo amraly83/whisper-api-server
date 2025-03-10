c8c85af15b808383f0fceed2c2bdd5795f166bc0
Amr Rady <66267712+amraly83@users.noreply.github.com>
2025-03-10 12:49:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    whisper_model_size: str = "small"
+    whisper_model_size: str = "base"
     compute_type: str = "int8"
     device: str = "cpu"
-    beam_size: int = 3
+    beam_size: int = 2
 
     model_config: SettingsConfigDict = SettingsConfigDict(
         extra="ignore"  # Ignore unexpected environment variables
