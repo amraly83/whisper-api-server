edb52d28866ba786558be77456990e1d76708f30
amraly83 <66267712+amraly83@users.noreply.github.com>
2025-03-10 20:36:19 +0300

    update

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    device: str = "cuda"
     whisper_model_size: str = "base"
     api_key: str
     max_duration: int = 600
